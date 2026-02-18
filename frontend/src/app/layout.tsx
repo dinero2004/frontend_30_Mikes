3e1bd3f4ece5ed3ceec7cdb38ab14d01507ed671
Lazar <lazar.minkov@gmail.com>
2026-02-18 19:36:07 +0100

    added the font-family to layout.tsx

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,7 +1,13 @@
 import { SessionProvider } from "next-auth/react";
+import localFont from "next/font/local";
 import { auth } from "@/auth";
 import { ReactNode } from "react";
 import "./globals.css";
+
+const capture = localFont({
+  src: "@/fonts/Capture_It.ttf",
+  display: "swap",
+});
 
 export default async function RootLayout({
   children,
@@ -11,7 +17,7 @@ export default async function RootLayout({
   const session = await auth();
 
   return (
-    <html lang="en">
+    <html lang="en" className={capture.className}>
       <body>
         <SessionProvider session={session}>
           {children}
